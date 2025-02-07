d7209539771de31a1dac189befa610a1cc3c2bc6
unknown <arora.manali@gmail.com>
2025-02-07 14:59:41 +0100

    update

---
+++ src/knowledge_graph/__init__.py
@@ -0,0 +1,11 @@
+"""
+knowledge_graph package
+
+This package contains modules for constructing and managing the Neo4j knowledge graph.
+It provides the following public function:
+  - import_graph: Executes the offline import process into Neo4j.
+"""
+
+from .knowledge_graph import main as import_graph
+
+__all__ = ['import_graph']
